f0debce572129e109fd9fe02e130103feedd337f
slimhippo <lzl0120@gmail.com>
2015-01-22 11:16:48 +0800

    MODIFY

--- umeng/main.py
+++ umeng/main.py
@@ -5,57 +5,28 @@ from time import sleep
 import time
 import umeng_config
 import edit_build_prop
+from shuaji import exec_adb_cmd
+import shuaji
 
-def shuaji():
-#	(status, result) = commands.getstatusoutput("adb install -r LoveBirds_normao2.apk")
-#	if status == 0:
-#		print("安装成功")
-#	else:
-#		print("安装失败")
-#		print(result)
-#		return
-	print("打开应用")
-	(status, result) = commands.getstatusoutput("adb shell am start -n com.zoneol.lovebirds/.ui.WelcomeActivity")
-	if status == 0 :
-		print("打开应用成功")
-		print("等待 20s")
-		time.sleep(20)
-	else:
-		print("打开应用失败")
-		print(result)
-		return
-	
-	print("清理应用缓存")
-	(status, result) = commands.getstatusoutput("adb shell pm clear com.zoneol.lovebirds")
-	if status == 0 :
-		print("清理应用缓存成功")
-		time.sleep(5)
-	else:
-		print("清理应用缓存失败")
-		return
-	print("执行完成")
 	
 print("开始执行")
 count = 0
 while count < umeng_config.AMOUNT_OF_DEVICES :
 	count += 1
 	time_count = 0
+	print("更新build.prop")
 	edit_build_prop.update_build_prop()
-	print("将build.prop push到/sdcard/中")
-	
-	sleep(10)
-	
-	(status, result) = commands.getstatusoutput("adb push build.prop /sdcard/")
-	sleep(10)
-	
-	(status, result) = commands.getstatusoutput("adb shell am start -n com.example.mountsystem/.MainActivity")
-	if status == 0:
-		print("mount /system and modify build.prop success")
-	else:
-		print("mount /system and modify build.prop failed")
+
+	result = exec_adb_cmd(umeng_config.PUSH_BUILD_PROP_CMD,"将build.prop push到/sdcard/中",10)
+	if result != 0:
 		exit()
-	(status, result) = commands.getstatusoutput("adb disconnect")	
-	sleep(20)
+		
+	result = exec_adb_cmd(umeng_config.LANUCN_MOUNT_SYSTEM_CMD,"启动mountsystem app，将修改过的build.prop 重定向到/system/build.prop中",10)
+	if result != 0:
+		exit()
+	
+	exec_adb_cmd(umeng_config.DISCONNECT_ADB_CMD,"disconnect adb",0)
+
 	
 	print("正在重启。。。。")
 	sleep(40)
@@ -63,10 +34,12 @@ while count < umeng_config.AMOUNT_OF_DEVICES :
 	print("连接adb")
 	connect_time = 0
 	
-	(status,result) = commands.getstatusoutput("adb kill-server")
+	(status,result) = commands.getstatusoutput(umeng_config.DISCONNECT_ADB_CMD)
+	sleep(5)
+	(status,result) = commands.getstatusoutput(umeng_config.ADB_KILLSERVER_CMD)
 	
 	while True:
-		(status,result) = commands.getstatusoutput("adb connect 192.168.1.101")
+		(status,result) = commands.getstatusoutput(umeng_config.CONNECT_ADB)
 		if status == 0:
 			connect_time += 1;
 			print("连接adb成功")
@@ -78,8 +51,9 @@ while count < umeng_config.AMOUNT_OF_DEVICES :
 			print("连接adb失败，重试")
 			continue
 	
-	while time_count < umeng_config.TIME_FOR_EACH_DEVICE :
-		time_count = time_count + 1
-		shuaji()
-		sleep(30)
+#	while time_count < umeng_config.TIME_FOR_EACH_DEVICE :
+#		time_count = time_count + 1
+	for n in range(umeng_config.TIME_FOR_EACH_DEVICE):
+		shuaji.shuaji()
+		sleep(10)
 	
--- umeng/umeng_config.py
+++ umeng/umeng_config.py
@@ -1,4 +1,5 @@
 #coding=utf-8
+
 #每次刷数据的机型数量
 AMOUNT_OF_DEVICES = 20
 
@@ -16,3 +17,29 @@ PATH_BUILD_PROP = "build.prop"
 
 #build.prop中"ro.product.model"所在行的序号，从0开始算
 DEVICE_NAME_LINE_NUM = 14
+
+#如果使用TCP adb连接电脑，请在这里配置ip
+TCP_ADB_ADDR = "192.168.1.101"
+
+#刷数据用到的命令
+#启动应用
+LAUCH_APP_CMD = "adb shell am start -n com.zoneol.lovebirds/.ui.WelcomeActivity"
+
+#清理应用缓存
+CLEAN_APP_CACHE_CMD = "adb shell pm clear com.zoneol.lovebirds"
+
+#将build.prop push到 /sdcard中
+PUSH_BUILD_PROP_CMD = "adb push build.prop /sdcard/"
+
+#启动mountsystem app
+LANUCN_MOUNT_SYSTEM_CMD = "adb shell am start -n com.example.mountsystem/.MainActivity"
+
+#disconnect adb
+DISCONNECT_ADB_CMD = "adb disconnect"
+
+#adb killserver
+ADB_KILLSERVER_CMD = "adb killserver"
+
+#adb connect
+CONNECT_ADB = "adb connect %s" % (TCP_ADB_ADDR,)
+
